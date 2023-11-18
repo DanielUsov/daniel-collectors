8103f2ed90f0480eee82e9b64aad66cd62738d93
Daniel Usov <dan.usov82@gmail.com>
2023-11-18 11:17:29 +0300

    restructure files

---
+++ webpack/classic/src/index.js
@@ -0,0 +1,6 @@
+import html from './README.md';
+
+let container = document.getElementById('title');
+console.log(container);
+
+container.innerHTML = html;
---
+++ webpack/classic/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'markdown-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
