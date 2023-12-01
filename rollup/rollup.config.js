eb3970c05c1522a1327254c9bd03676ab0bbc954
Daniel <91885191+DanielUsov@users.noreply.github.com>
2023-12-01 11:43:18 +0300

    feat: task 7 (#5)
    Co-authored-by: Daniel <dan.usov82@gmail.com>

---
+++ rollup/rollup.config.js
@@ -0,0 +1,21 @@
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: './src/index.js',
+  output: {
+    format: 'cjs',
+    file: 'dist/index.js',
+    name: 'build',
+  },
+  plugins: [
+    resolve(),
+    copy({
+      targets: [
+        { src: 'src/*.html', dest: 'dist' },
+        { src: 'src/*.css', dest: 'dist' },
+      ],
+    }),
+  ],
+};
---
+++ rollup/src/index.js
@@ -0,0 +1,5 @@
+const greet = () => {
+  return 'Hello wolrd!';
+};
+
+export default greet;
