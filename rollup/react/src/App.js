43bc38c2c99b76f630d5180251e5d1d5068a277c
Daniel Usov <dan.usov82@gmail.com>
2023-12-02 17:47:44 +0300

    feat: task 8

---
+++ rollup/react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ReactComponent as SampleImage } from './image/genetic-data-svgrepo-com.svg';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World From React-Rollup</h1>
+      <SampleImage />
+    </>
+  );
+}
+
+export default App;
